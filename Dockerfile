e562280f7be086c3cc9d6a6015b04ef91c0987ce
EmitoDilaMorte <emiliano.aristarodriguez@gmail.com>
2026-03-27 19:32:02 -0600

    Desarrollo de home: primera parte, uso de plantilla, limpieza de archivos y carpetas

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM node:20-alpine AS base
+FROM node:22-bookworm-slim AS base
 
 WORKDIR /app
 
 COPY package*.json ./
-RUN npm ci
+RUN npm install
 
 COPY . .
 
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,51 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+    'motion-v/nuxt'
+  ],
+
+  devtools: {
+    enabled: true
+  },
+
+  css: ['~/assets/css/main.css'],
+
+  site: {
+    url: process.env.NUXT_SITE_URL || 'https://example.com'
+  },
+
+  compatibilityDate: '2024-11-01',
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/'
+      ],
+      crawlLinks: true
+    }
+  },
+
+  vite: {
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit'
+      ]
+    }
+  },
+
+  eslint: {
+    config: {
+      stylistic: {
+        commaDangle: 'never',
+        braceStyle: '1tbs'
+      }
+    }
+  }
 })
