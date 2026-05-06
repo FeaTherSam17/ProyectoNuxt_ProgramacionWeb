8956e3b504bac838bc4d40cc417568507c6f7232
clu789 <mil.rinzler@gmail.com>
2026-05-05 21:57:49 -0600

    des2

--- app/app.vue
+++ app/app.vue
@@ -17,7 +17,7 @@ const loadSearchSections = () => {
     : (queryCollectionSearchSections as any)('blog')
 }
 
-useHead({
+useHead({ 
   meta: [
     { charset: 'utf-8' },
     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
